41bfd8b4308df83bc7693d20b5a7e5e1a6d9abfa
Seshan <zeshan.nandan@gmail.com>
2019-03-10 23:57:24 +0530

    Constants defined

---
+++ lib/assets/constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class WidgetColors {
+  static Color get primaryColor => const Color(0xFFFF4C00);
+  static Color get primaryDark => const Color(0xFFDB4100);
+  static Color get primaryDarker => const Color(0xFFA73200);
+
+  static Color get secondaryColor => const Color(0xFF);
+
+  static Color get black => const Color(0xFF333333);
+  static Color get grey => const Color(0xFFF4F4F4);
+  static Color get transparent => const Color(0x00000000);
+}
+
+class Dimensions {
+  static double get small => 4.0;
+  static double get medium => 8.0;
+  static double get large => 16.0;
+  static double get extraLarge => 16.0;
+}
+
+class TextSize {
+  static double get small => 8.0;
+  static double get medium => 10.0;
+  static double get large => 14.0;
+  static double get extraLarge => 18.0;
+}
---
+++ lib/pages/landing.dart
@@ -0,0 +1,12 @@
+//https://drive.google.com/open?id=1FwiUFmW9gZYKsx0Lno0h9h3OtzDbdigi
+
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
